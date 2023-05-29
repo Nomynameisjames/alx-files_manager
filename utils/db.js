5836a80a55541ee14805f3ab3d32a28a6f2e031b
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-29 22:26:24 +0100

    mongoDB client

--- utils/db.js
+++ utils/db.js
@@ -18,6 +18,7 @@ const { MongoClient } = require('mongodb');
 class DBClient {
   constructor() {
     this.host = process.env.DB_HOST || 'localhost';
+ 
     this.port = process.env.DB_PORT || 27017;
     this.database = process.env.DB_DATABASE || 'files';
     this._manager = {};
