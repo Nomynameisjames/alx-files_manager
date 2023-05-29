c83746632047eb88abeb4113442c85e44fc9a984
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-29 23:05:11 +0100

    mongoDB client

--- utils/db.js
+++ utils/db.js
@@ -1,46 +1,49 @@
 /*
  * objective -> create a file db.js that contains the class DBClient.
  * the constructor that creates a client to MongoDB:
-     host: from the environment variable DB_HOST or default: localhost
-     port: from the environment variable DB_PORT or default: 27017
-     database: from the environment variable DB_DATABASE or default: files
-     _manager
+      host: from the environment variable DB_HOST or default: localhost
+      port: from the environment variable DB_PORT or default: 27017
+      database: from the environment variable DB_DATABASE or default: files
+      _manager
  * a function isAlive that returns true when the connection to MongoDB is a
-    success otherwise, false
+     success otherwise, false
  * an asynchronous function nbUsers that returns the number of documents in the
-   collection users
+     collection users
  * an asynchronous function nbFiles that returns the number of documents in the
-   collection files
- */
+    collection files
+*/
+import { MongoClient } from 'mongodb';
 
-const { MongoClient } = require('mongodb');
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this._manager = {};
-    this._connectToDB();
-  }
-
-  async _connectToDB() {
-    const client = await MongoClient.connect(`mongodb://${this.host}:${this.port}`,
-      { useUnifiedTopology: true });
-    this._manager = client.db(this.database);
+    this.session = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.session.connect().then(() => {
+      this.db = this.session.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   isAlive() {
-    return !!this._manager;
+    return this.session.isConnected();
   }
 
   async nbUsers() {
-    return this._manager.collection('users').countDocuments();
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
   }
 
   async nbFiles() {
-    return this._manager.collection('files').countDocuments();
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
   }
 }
 
