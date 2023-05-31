eff350922c25ec80773000566174e9b5d08af66d
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-30 17:23:26 -0700

    auth route

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -30,16 +30,16 @@ class AuthController {
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [email, password] = credentials.split(':');
     if (!email || !password) {
-      return response.status(401).send({ error: 'Unauthorized' });
+      return response.status(401).json({ error: 'Unauthorized' });
     }
     const user = await dbClient.db.collection('users').findOne({ email, password: sha1(password) });
     if (!user) {
-      return response.status(401).send({ error: 'Unauthorized' });
+      return response.status(401).json({ error: 'Unauthorized' });
     }
     const token = uuidv4();
     const key = `auth_${token}`;
     await redisClient.set(key, user._id.toString(), 86400);
-    return response.status(200).send({ token });
+    return response.status(200).json({ token });
   }
 
   static async getDisconnect(request, response) {
@@ -48,10 +48,10 @@ class AuthController {
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
     if (!userId) {
-      return response.status(401).send({ error: 'Unauthorized' });
+      return response.status(401).json({ error: 'Unauthorized' });
     }
     await redisClient.del(key);
-    return response.status(204).send();
+    return response.status(204).json();
   }
 }
 
